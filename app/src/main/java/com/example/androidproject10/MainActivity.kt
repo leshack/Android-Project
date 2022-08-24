447b259f3a02e52dc278f5a8e33e6f2cc423f725
Lesley Tulienge <kesleytulienge@gmail.com>
2022-08-24 20:00:23 +0300

    Initial commit Android

---
+++ app/src/main/java/com/example/androidproject10/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.androidproject10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
